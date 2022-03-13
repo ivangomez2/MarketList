482320485d9528e1b7f4328446ee65f5462dd13a
ivan <ivansangomez2@gmail.com>
2022-03-13 17:32:40 -0300

    agregando delete

--- index.js
+++ index.js
@@ -14,13 +14,12 @@ cfinalButton.addEventListener("click",buttonPFinal)
 
 let divCostoFinal = document.getElementById("div__costoFinal")
 
+
+
+
 const costos = []
 let precioFinal = 0
 
-
-const domEl = ()=>{
-  
-}
 
 
 function buttonPFinal() {
@@ -42,30 +41,33 @@ function FormuData(el){
     let section__prod = document.createElement("div")
     section__prod.className = "div__prod"
     let totalPrice = productos.cost * productos.cant
-    section__prod.innerHTML = `<img src="./img/carrito2.png"><ul><li>Producto:${productos.name}</li><li>Cantidad:${productos.cant}</li><li>Precio Total:$${totalPrice}</li><ul>`
-    
-    
+    section__prod.innerHTML = `<img src="./img/carrito2.png"><ul><li>Producto:${productos.name}</li><li>Cantidad:${productos.cant}</li><li>Precio Total:$${totalPrice}</li></ul>`
+ 
+
+    /*funcion para eliminar cada elemento de mi tabla*/
+
+    let eliminar = document.createElement("button")
+    eliminar.innerText = "Eliminar"
+    section__prod.appendChild(eliminar)
+
+    eliminar.addEventListener("click", (event) =>{
+     event.target.parentNode.remove()
+     
+   
+    })
 
     /*calcular precio total*/
     costos.push(totalPrice)
     console.log(elemento.appendChild(section__prod))
   
     /* usar esta funcion para sumar costo total*/
-  
     precioFinal= 0
     for (let i = 0; i < costos.length; i++) {
       precioFinal += costos[i]
-  }}
+   }
+ }
 
-
-/*probar map y despues filter*/
-
-const mapa = ()=>{
- let filtrarPorNombre = productos.filter = ()=>{
-    return productos.includes("a")
-  }
-}
-
+  console.log(costos)
 
 
 buttonPFinal()
