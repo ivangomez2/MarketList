c4c984fdbc09a1bf42c9782bc0c0b1615f9e9015
ivan <ivansangomez2@gmail.com>
2022-03-21 22:38:15 -0300

    add

--- index.js
+++ index.js
@@ -8,17 +8,31 @@ let cfinalButton = document.getElementById("calcButton")
 cfinalButton.addEventListener("click",buttonPFinal)
 
 
-
 let divCostoFinal = document.getElementById("div__costoFinal")
 
-const productos = [{name:"", cant:"",cost:""}]
+
+class Productos {
+  constructor(name,cant,cost){
+    this.name = name;
+    this.cant = cant;
+    this.cost = cost;
+  }
+}
+
+
 
 const prodsDelete = []
 let costosBorrados = 0
-
+let productosAgregados = []
 let costos = []
 let precioFinal = 0
-console.log(costos)
+
+
+let botonBuscar = document.getElementById("botonBuscar")
+let inputBuscar = document.getElementById("inputBuscar")
+
+
+
 
 
 function buttonPFinal() {
@@ -39,15 +53,47 @@ function FormuData(el){
     el.preventDefault()
     let form = el.target
     
-    productos.name = form.children[1].value
-    productos.cant = form.children[3].value
-    productos.cost = form.children[5].value
+
+    const products = form.children[1].value;
+    const quantity = form.cant.value;
+    const cost = form.cost.value;
+   let prodNew = new Productos (products,quantity,cost)
+   productosAgregados = prodNew
+
+   
+   let arrayNew = Object.keys(prodNew).map(function(key) {
+   return prodNew[key];
     
+   
+});
+   
+
+
+   botonBuscar.addEventListener("click",res)
+   
+   function res(){
+    let valor = inputBuscar.value
+    
+    let doc = document.getElementById("div__buscador")
+     let buscar = arrayNew.find(i=> i === valor )
+     console.log("el producto existe = ", buscar)
+   
+     let newEl = document.createElement("div")
+     newEl.innerHTML = `<li>${buscar}</li>`
+    
+   }
+
+ 
+   
+
+
+
     let section__prod = document.createElement("div")
     section__prod.className = "card addCard"
-    let totalPrice = productos.cost * productos.cant
-    section__prod.innerHTML = `<img src="./img/carrito2.png"><ul><li>Producto:${productos.name}</li><li>Cantidad:${productos.cant}</li><li>Precio Total:$${totalPrice}</li></ul>`
+    let totalPrice = cost * quantity
+    section__prod.innerHTML = `<img src="./img/carrito2.png"><ul><li>Producto: ${products} </li><li>Cantidad:${quantity}</li><li>Precio Total:$${totalPrice}</li></ul>`
  
+    
 
     /*funcion para eliminar cada elemento de mi tabla*/
 
@@ -58,20 +104,22 @@ function FormuData(el){
 
     eliminar.addEventListener("click", (event) =>{
      let ver = event.target.parentNode.remove()
+     console.log(ver)
     prodsDelete.push(totalPrice)
     console.log(prodsDelete)
-     
+    arrayNew = []
     })
 
     /*calcular precio total*/
-    costos.push(totalPrice)
+   costos.push(totalPrice)
     console.log(elemento.appendChild(section__prod))
   
-    /* usar esta funcion para sumar costo total*/
+   //  usar esta funcion para sumar costo total
     precioFinal= 0
     for (let i = 0; i < costos.length; i++) {
       precioFinal += costos[i]
    }
+
 
 
   }
@@ -86,23 +134,47 @@ function deleteAll() {
   DeleteAllBtn.addEventListener("click",borrarTodo)
   
   function borrarTodo() {
+   
+    if(costos.length > 0){
+
+      
     let divs =  elemento.getElementsByClassName("card")
-    
+    let arra = arrayNew.length = 0
+    console.log(arra)
     while(divs.length > 0){                   
       elemento.removeChild(divs[0]);
       precioFinal = 0;
+      costos = []  
       
+    }
+    
+  }else{
+    costos = 0
+    
+    alert("No tienes productos agregados aún")
   }
+ divCostoFinal.innerHTML = `<p>Costo total: $${precioFinal}</p>`
 }
 }
+
+
+ 
 
 function main(){
 
   deleteAll()
   buttonPFinal()
-
+  
 }
 
 
 main()
+
+
+/*   agregar cantidad de productos seleccionados
+let e = document.createElement("div")
+e.innerHTML = `<h4>${costos.length}</h4>`
+priceFinalSection.append(e)
+console.log(e)*/
+
 
